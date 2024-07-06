241f60e8bbb052b1df3e6a9e92b3790cd646f44f
shubham <shubhamwable0198@gmail.com>
2024-07-06 16:03:42 +0530

    addview remove

---
+++ pro1/app1/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, redirect
+from . forms import EmployeeForm
+from . models import Employee
+from django.contrib.auth.decorators import login_required
+# Create your views here.
+
+
+def showview(request):
+    obj = Employee.objects.all()
+    return render(request, "app1/show.html", {"emp": obj})
+
+@login_required(login_url="login")
+def updateview(request, pk):
+    obj = Employee.objects.get(eid=pk)
+    form = EmployeeForm(instance=obj)
+    if request.method == "POST":
+        form = EmployeeForm(request.POST, instance=obj)
+        if form.is_valid():
+            form.save()
+            return redirect("show")
+
+    return render(request, "app1/add.html", {"form": form})
+
+@login_required(login_url="login")
+def deleteview(request, pk):
+    obj = Employee.objects.get(eid=pk)
+    obj.delete()
+    return redirect("show")
+
